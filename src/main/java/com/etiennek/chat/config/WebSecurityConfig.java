d9a0da3862d96aaeb1e8e4659d8c80b48672e644
EtienneK <e@ek.pe>
2015-01-13 07:47:39 +0200

    Still working on login screen

--- src/main/java/com/etiennek/chat/config/WebSecurityConfig.java
+++ src/main/java/com/etiennek/chat/config/WebSecurityConfig.java
@@ -3,9 +3,13 @@ package com.etiennek.chat.config;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
@@ -19,9 +23,21 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("admin")
-				.roles("ADMIN", "USER").and().withUser("user").password("user")
-				.roles("USER");
+		auth.authenticationProvider(new AuthenticationProvider() {
+			@Override
+			public boolean supports(Class<?> authentication) {
+				return UsernamePasswordAuthenticationToken.class
+						.isAssignableFrom(authentication);
+			}
+
+			@Override
+			public Authentication authenticate(Authentication authentication)
+					throws AuthenticationException {
+				UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+				return new UsernamePasswordAuthenticationToken(token.getName(),
+						token.getCredentials(), null);
+			}
+		});
 	}
 
 }
--- src/main/java/com/etiennek/chat/web/ChatController.java
+++ src/main/java/com/etiennek/chat/web/ChatController.java
@@ -2,15 +2,15 @@ package com.etiennek.chat.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/chat")
 public class ChatController {
-	
-	@RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
-	
+
+	@RequestMapping("/room")
+	ModelAndView index() {
+		return new ModelAndView("chat/room");
+	}
+
 }
